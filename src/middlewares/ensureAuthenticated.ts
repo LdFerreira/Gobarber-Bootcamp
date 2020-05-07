2bd9486e251247688afd1490b1a3108bb2a2937b
Leandro Ferreira <ldrsiqueira@gmail.com>
2020-05-07 15:43:22 -0300

    first commit

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from 'express';
+
+export default function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): void {
+  const authHeader = request.headers.authorization;
+}
